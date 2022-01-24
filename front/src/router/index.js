b23c61a34677a265f4eab43501789bf53dd21025
lsh <jetaime12121@gmail.com>
2022-01-24 18:24:10 +0900

    view setting

--- front/src/router/index.js
+++ front/src/router/index.js
@@ -1,28 +1,30 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
-import QrReader from '../views/QrReader.vue'
+import MainPage from '../views/MainPage.vue'
+import Home from '../views/test/Home.vue'
+import Generate from '../views/test/Generate.vue'
+import QrReader from '../views/test/QrReader.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
+    name: 'MainPage',
+    component: MainPage
+  },
+  {
+    path: '/test',
     name: 'Home',
     component: Home
   },
   {
-    path: '/generate',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: function () {
-      return import(/* webpackChunkName: "about" */ '../views/About.vue')
-    }
+    path: '/test/generate',
+    name: 'Generate',
+    component: Generate
   },
   {
-    path: '/reader',
+    path: '/test/reader',
     name: 'QrReader',
     component: QrReader
   },
