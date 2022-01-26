88a5def30a8088dabdc7b4a16bbbd49f4d2e381a
yoonhocho <yoonhocho91@gmail.com>
2022-01-26 16:39:39 +0900

    qr스캐너 페이지

--- front/src/components/user/QrShow.vue
+++ front/src/components/user/QrShow.vue
@@ -1,14 +1,28 @@
 <template>
     <div class="wrapAll">
         <div class="logo">
-            <p>기린아</p>
+            <p>기린아 강릉투어</p>
         </div>
-        <div class="qrSec">
-            <qr-gen></qr-gen>
+        <!-- // logo end -->
+
+        <div class="section">
+            <!-- <div class="timer">
+                <p>48:00:00</p>
+            </div> -->
+            <!-- //timer end -->
+            <div class="qrSec">
+                <qr-gen></qr-gen>
+                <p class="num">2022012611111</p>
+            </div>
+            <!-- // qrSec end -->
+
+            <div class="userInfo">
+                <p class="name">구매자 이름 : 이승헌</p>
+                <p class="phoneNum">휴대폰번호 : 01011114444</p>
+            </div>
+            <!-- // userInfo end -->
         </div>
-        <div class="userInfo">
-            
-        </div>
+        <!-- // section end -->
     </div>
     
 </template>
@@ -23,8 +37,17 @@ export default {
 </script>
 
 <style scoped>
-    .wrapAll {overflow: hidden; width: 60%; background-color: #fff; text-align: center; margin: 0 auto;}
-    .wrapAll .logo {float: right; margin:  0 50px 0;}
+    .wrapAll {overflow: hidden; width: 30%; background-color: #FF6A6A; text-align: center; margin: 0 auto;}
+    .wrapAll p {color: #fff; font-size: 20px;}
 
-    .wrapAll .qrSec {float: center; margin: 100px 0 0;}
+    .wrapAll .logo {float: right; margin: 0 50px 0;}
+    .wrapAll .logo p {font-size: 30px;}
+
+    .wrapAll .section .timer {margin: 50px 0;}
+    .wrapAll .section .timer p {font-size: 60px;}
+
+    .wrapAll .section .qrSec {float: center; margin: 50px 0; }
+    .wrapAll .section .qrSec .num {margin: 20px 0; font-weight: 600;}
+
+    .wrapAll .section .userInfo {}
 </style>
