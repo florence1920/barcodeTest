1ecb5b5501ffd5c3d8859ccdef2254ce63916f89
florence1920 <jetaime12121@gmail.com>
2022-01-26 15:41:47 +0900

    link

---
+++ front/src/components/test/LinkGen.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+      <div class="box" v-on:click="linkGenerator">
+        링크 생성
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            key: 'value'
+        }
+    },
+    methods: {
+        linkGenerator() {
+            console.log('click');
+        }
+    },
+}
+</script>
+
+<style scoped>
+    .box { padding:0 0 0 45px; margin:50px 0; width: 160px; height: 100px; line-height: 100px; background: #42b983; color: rgb(202, 202, 202); font-weight: bold; border-radius: 3px; cursor: pointer;}
+    .box:hover {background: #00ff8c; color: #fff;}
+</style>
--- front/src/views/test/Home.vue
+++ front/src/views/test/Home.vue
@@ -1,17 +1,20 @@
 <template>
   <div class="home">
     <HelloWorld/>
+    <link-gen></link-gen>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/test/HelloWorld.vue'
+import LinkGen from '../../components/test/LinkGen.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    HelloWorld,
+    LinkGen
   }
 }
 </script>
